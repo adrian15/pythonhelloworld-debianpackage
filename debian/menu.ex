b4b0982434e5ccef678393792c9631a477a3e8d5
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2017-05-13 18:30:33 +0200

    Added default files created by dh_make Later on we will remove files we do not need and we will rename some files if needed.

---
+++ debian/menu.ex
@@ -0,0 +1,2 @@
+?package(pythonhelloworld-debianpackage):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="pythonhelloworld-debianpackage" command="/usr/bin/pythonhelloworld-debianpackage"
---
+++ debian/pythonhelloworld-debianpackage.default.ex
@@ -0,0 +1,10 @@
+# Defaults for pythonhelloworld-debianpackage initscript
+# sourced by /etc/init.d/pythonhelloworld-debianpackage
+# installed at /etc/default/pythonhelloworld-debianpackage by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
