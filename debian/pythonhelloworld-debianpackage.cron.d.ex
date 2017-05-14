07fbe1f62d869f1b21901420094211fe6fefcc42
Adrian Gibanel Lopez <adrian.gibanel@btactic.com>
2017-05-14 19:52:31 +0200

    Remove non required debian/ files.
    From dh_make result we remove debian/ files
    that are not required.
    
    Required files are:
    control
    copyright
    changelog
    rules

--- debian/pythonhelloworld-debianpackage.cron.d.ex
+++
@@ -1,4 +0,0 @@
-#
-# Regular cron jobs for the pythonhelloworld-debianpackage package
-#
-0 4	* * *	root	[ -x /usr/bin/pythonhelloworld-debianpackage_maintenance ] && /usr/bin/pythonhelloworld-debianpackage_maintenance
